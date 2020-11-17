32cbf6e65836407d5cd16ff524e63e7380ea70f6
alexxx-ivanow <alexxx-ivanow@yandex.ru>
2020-11-17 10:13:13 +0300

    Update form_wrap.php

--- form_wrap.php
+++ form_wrap.php
@@ -1,11 +1,11 @@
 <div class="oredr-modal jsModalContent" id="oredr-modal">
     {'!AjaxForm' | snippet : [
-        'form' => 'moesk_form',
+        'form' => 'form',
         'hooks' => 'email,createNewOrderFromService',
-        'emailTpl' => 'email_moesk_form',
+        'emailTpl' => 'email_form',
         'emailFrom' => $_modx->config.emailsender,
-        'emailTo' => 1 | resource : 'ppa_email_form',
-        'emailSubject' => 'Заявка со страницы услуги',
+        'emailTo' => 1 | resource : 'email_form',
+        'emailSubject' => 'Заявка со страницы',
         'customValidators' => 'formit2checkPhone,formit2checkfiles,formit2checkfilesMoesk,formit2checkEmail',
         'validate'=>'inn:required,belongs:required,fio:required,phone:required:formit2checkPhone,email:required:formit2checkEmail,agreement:required,check_files_size:formit2checkfiles:formit2checkfilesMoesk,property_any_electro:required',
     	'validationErrorMessage'=>'В форме содержатся ошибки!',
